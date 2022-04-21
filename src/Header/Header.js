0de68acf127fc9bf839d7c6c7ee2ff5526170c8b
sina <sina99.sn@gmail.com>
2022-04-21 15:50:23 +0430

    watchlist added

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,21 +1,22 @@
 import React, { useEffect } from 'react'
-<<<<<<< HEAD
 import './Header.css'
-import '../Styles.css'
 import 'bootstrap/dist/css/bootstrap.css';
+import logo from '../Images/IEMDB.png'
+import profile_icon from '../Images/profile.jpg'
+import '../Styles.css'
 
 function Header(props) {
     return (
         
         <div class="navbar" id="navigation">
         <a class="navbar-brand">
-            <img src="/Images/IEMDB.png" width="60" alt=""/>
+            <img src={logo} width="60" alt=""/>
         </a>
 
         <div class="profile-navbar">
             <a class="navbar-brand">
                 <div class="dropdown">
-                    <img src="/Images/profile.jpg" class="profile" alt="" width="60"/>
+                    <img src={profile_icon} class="profile" alt="" width="60"/>
                     <div class="dropdown-content">
                         <a href="#">salehi@gmail.com</a>
                         <a href="#">watch list</a>
@@ -29,34 +30,3 @@ function Header(props) {
 }
 
 export default Header
-=======
-import logo from '../Images/IEMDB.png'
-import profile_icon from '../Images/profile.jpg'
-import './Header.css'
-import '../Styles.css'
-
-function Header(props) {
-    return (
-        <div class="navbar" id="navigation">
-            <a class="navbar-brand">
-                <img src={logo} width="60" alt=""/>
-            </a>
-
-            <div class="profile-navbar">
-                <a class="navbar-brand">
-                    <div class="dropdown">
-                        <img src={profile_icon} class="profile" alt="" width="60"/>
-                        <div class="dropdown-content">
-                            <a href="#">salehi@gmail.com</a>
-                            <a href="#">watch list</a>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
-
-    )
-}
-
-export default Header
->>>>>>> d0933cdde056ea7e178352b117f5513d1f2caf30
